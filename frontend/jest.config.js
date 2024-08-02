812aab240eb95b6ba3b85ae7f050c4361f6f18f0
kokwatan <kokwatan@yahoo-corp.jp>
2024-08-03 02:42:27 +0900

    テストとdevelopが両方できる

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  testPathIgnorePatterns: ["node_modules", ".cache", "public"],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+    ".+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/__mocks__/file-mock.js`,
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
---
+++ frontend/src/components/common/__tests__/Footer.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Footer from "../Footer";
+
+test("Footer renders correctly", () => {
+  const { getByText } = render(<Footer />);
+  const footerElement = getByText(
+    /© 2024 KOKI Watanabe. All rights reserved./i,
+  );
+  expect(footerElement).toBeInTheDocument();
+});
