e39e3cc78667051c51c9e5c37d12197defbbfa08
koki watanabe <address8128@gmail.com>
2025-09-20 17:52:46 +0900

    テスト修正

--- frontend/src/components/SudokuBoard.tsx
+++ frontend/src/components/SudokuBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { SudokuBoard as SudokuBoardType } from '../types/sudoku'
 
 interface SudokuBoardProps {
